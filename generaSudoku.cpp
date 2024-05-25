5b75b633ef6fdfcecb9211e75fa772063de46e56
end <72604018+laok233@users.noreply.github.com>
2024-05-25 22:20:00 +0200

    fix sudokuSoluzioni

--- generaSudoku.cpp
+++ generaSudoku.cpp
@@ -14,6 +14,7 @@ void initMatrice(int mat[DIM][DIM]) {
     }
 }
 
+// effettua i controlli. controlla che il numero sia valido nella cella.
 bool controlloNumero(int mat[DIM][DIM], int rig, int col, int num) {
     // controllo riga
     for (int i = 0; i < DIM; i++) {
@@ -50,28 +51,41 @@ bool controlloNumero(int mat[DIM][DIM], int rig, int col, int num) {
 }
 
 // utilizza il backtracking per cercare di generare il sudoku.
+// puo essere usato anche per risolvere un sudoku
+// è una funzione ricorsiva, vuol dire che la funzione chiama se stessa.
 bool risolviSudoku(int mat[DIM][DIM], bool risoluzioneCPU) {
     int rig, col;
+
+    // si pressupone che la matrice sia piena all'inizio
     bool matriceVuota = false;
 
+    // vedere se ci sono delle celle vuote
     for (rig = 0; rig < DIM; rig++) {
         for (col = 0; col < DIM; col++) {
             if (mat[rig][col] == 0) {
+
+                // trovato una cella vuota
+                // viene aggiornato la variabile
                 matriceVuota = true;
                 break;
             }
         }
         if (matriceVuota) {
+            // uscire prima dal ciclo for perché c'è una cella vuota
             break;
+
         }
     }
 
+    // verificare che la matrice sia piena
+    // se è piena si esce dalla funzione
     if (!matriceVuota) {
         return true;
     }
 
     int numeri[DIM] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    // i numeri vengono messi a caso solo quando ce la generazione del sudoku
     if (!risoluzioneCPU) {
         for (int i = 0; i < DIM; i++) {
             int j = rand() % 9;
@@ -79,18 +93,26 @@ bool risolviSudoku(int mat[DIM][DIM], bool risoluzioneCPU) {
         }
     }
 
+    // si prova ad inserire i numeri nella cella vuota seguendo le regole del sudoku
     for (int i = 0; i < 9; i++) {
         if (controlloNumero(mat, rig, col, numeri[i])) {
+            // il numero soddisfa le regole del sudoku, quindi viene inserito nella cella
             mat[rig][col] = numeri[i];
 
+            // si tenta di usare il numero e viene chiamata la funzione stessa per procedere
             if (risolviSudoku(mat, risoluzioneCPU)) {
+                // è stata trovata la soluzione, quindi restituisce true
                 return true;
             }
 
+            // è necessario ripristinare la cella perché nessun numero inserito soddisfa le regole del sudoku
+            // e si proverà con un altro numero nella cella precedente
             mat[rig][col] = 0;
         }
     }
 
+    // non è stato possibile trovare la soluzione, quindi restituisce false
+    // si procederà alla cella precedente e si proverà ad usare il numero successivo
     return false;
 }
 
@@ -103,27 +125,36 @@ void copiaMatrice(int mat1[DIM][DIM], int mat2[DIM][DIM]) {
     }
 }
 
+// funzione che cancella i numeri dalle celle in modo da rendere il sudoku giocabile 
+// mat è un sudoku completo già risolto
+// num è il numero dei numeri da togliere dalla matrice
 void cavaNumeri(int mat[DIM][DIM], int num) {
     do {
         // genera numeri da 0 a 8
         int rig = rand() % 9;
         int col = rand() % 9;
 
+        // cancella solo celle con numeri diversi da 0
         if (mat[rig][col] != 0) {
             mat[rig][col] = 0;
             num--;
         }
+
+        // continua a cancellare numeri finché non arriva a 0
     } while (num > 0);
 }
 
-
+// funzione ricorsiva che viene chiamata da verificaSudoku()
+// restituisce un booleano che indica se il sudoku ha più di una soluzione oppure no.
 bool sudokuSoluzioni(int mat[DIM][DIM], int& soluzioni, bool continuaRicerca = false) {
-    int rig, col;
+    int rig = -1, col = -1;
     bool matriceVuota = false;
 
-    for (rig = 0; rig < DIM; rig++) {
-        for (col = 0; col < DIM; col++) {
-            if (mat[rig][col] == 0) {
+    for (int i = 0; i < DIM; i++) {
+        for (int j = 0; j < DIM; j++) {
+            if (mat[i][j] == 0) {
+                rig = i;
+                col = j;
                 matriceVuota = true;
                 break;
             }
@@ -142,17 +173,16 @@ bool sudokuSoluzioni(int mat[DIM][DIM], int& soluzioni, bool continuaRicerca = f
         if (controlloNumero(mat, rig, col, num)) {
             mat[rig][col] = num;
 
-            if (sudokuSoluzioni(mat, soluzioni, continuaRicerca)) {
-                mat[rig][col] = 0;
-            } else {
-                return true;
+            if (!sudokuSoluzioni(mat, soluzioni, continuaRicerca)) {
+                return false;
             }
+
+            mat[rig][col] = 0; // Reset cella dopo ricorsione
         }
     }
 
-    return false;
+    return true;
 }
-
 // il sudoku deve avere una sola soluzione. quindi questa funzione serve a verificare la validità di un sudoku in base al numero possibile di soluzioni
 // se le soluzioni sono più di uno allora il sudoku non è valudo
 bool verificaSudoku(int mat[DIM][DIM]) {
